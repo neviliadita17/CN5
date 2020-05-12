3cff7969478d7708d348e172c51e32c9ef82362e
fitriaadinda <61970789+fitriaadinda@users.noreply.github.com>
2020-05-12 20:32:23 +0700

    intent implicit belum fix

--- app/src/main/java/com/example/cn5/AdapterNewsUsers.kt
+++ app/src/main/java/com/example/cn5/AdapterNewsUsers.kt
@@ -1,6 +1,8 @@
 package com.example.cn5
 
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,7 +10,9 @@ import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
 import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.item_news_list.*
 
 class AdapterNewsUsers(val mCtx: Context, val layoutResId: Int, val list: List<News> )
     : ArrayAdapter<News>(mCtx,layoutResId,list) {
@@ -19,6 +23,7 @@ class AdapterNewsUsers(val mCtx: Context, val layoutResId: Int, val list: List<N
 
         val textJudul = view.findViewById<TextView>(R.id.textJudul)
         val image = view.findViewById<ImageView>(R.id.img_item_photo)
+        val Read = view.findViewById<Button>(R.id.BtnRead)
         val news = list[position]
 
         textJudul.text = news.judul
@@ -31,7 +36,12 @@ class AdapterNewsUsers(val mCtx: Context, val layoutResId: Int, val list: List<N
             .error(R.color.colorPrimary)
             .into(image)
 
-
         return view
+
+        Read.setOnClickListener {
+            val webIntent: Intent = Uri.parse("https://www.android.com").let { webpage ->
+                Intent(Intent.ACTION_VIEW, webpage)
+            }
+        }
     }
 }
